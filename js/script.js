521569dd28adf26c3d661ff27e931dcb6c71ccc1
Angelo <christianangelor94@gmail.com>
2024-04-22 21:04:13 +0700

    feat: adding UI & simple calculation

--- js/script.js
+++ js/script.js
@@ -1,1 +1,16 @@
 // JS File
+
+function konversi() {
+    let celsius = document.getElementById("celsius").value;
+    let rumus = "(" + celsius +".9/5)+32"
+    let result = (celsius * 9 / 5) + 32;
+    console.log(result);
+    document.getElementById("fahrenheit").value = result
+    document.getElementById("rumus").value = rumus
+}
+
+function reset() {
+    document.getElementById("celsius").value = "";
+    document.getElementById("fahrenheit").value = "";
+    document.getElementById("rumus").value = "";
+}
