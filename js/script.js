5c203334084fdb6eec8feb8fb55ba6e5c5c76a85
Angelo <christianangelor94@gmail.com>
2024-04-25 17:55:24 +0700

    feat: change paragraph based on select

--- js/script.js
+++ js/script.js
@@ -1,16 +1,22 @@
 // JS File
 const ftoc = document.getElementById("f-to-c");
 const ctof = document.getElementById("c-to-f");
+const pc = document.getElementById("p-1")
+const pf = document.getElementById("p-2")
 
 document.getElementById("suhu-1").addEventListener("change", function () {
   const suhuu1 = document.getElementById("suhu-1").value;
   if (suhuu1 === "celsius") {
     ctof.style.display = "flex";
     ftoc.style.display = "none";
+    pc.style.display = "block"
+    pf.style.display = "none"
     document.getElementById("suhu-2").value = "fahrenheit";
   } else if (suhuu1 === "fahrenheit") {
     ftoc.style.display = "flex";
     ctof.style.display = "none";
+    pc.style.display = "none"
+    pf.style.display = "block"
     document.getElementById("suhu-2").value = "celsius";
   }
 });
@@ -20,10 +26,14 @@ document.getElementById("suhu-2").addEventListener("change", function () {
   if (suhuu2 === "celsius") {
     ftoc.style.display = "flex";
     ctof.style.display = "none";
+    pc.style.display = "none"
+    pf.style.display = "block"
     document.getElementById("suhu-1").value = "fahrenheit";
   } else if (suhuu2 === "fahrenheit") {
     ctof.style.display = "flex";
     ftoc.style.display = "none";
+    pc.style.display = "block"
+    pf.style.display = "none"
     document.getElementById("suhu-1").value = "celsius";
   }
 });
@@ -42,7 +52,7 @@ function konversi() {
       document.getElementById("rumus").value = rumus;
     } else if (suhu1 === "fahrenheit") {
       let input = document.getElementById("input").value;
-      let rumus = "5 / 9." + "(" + input + " - 32)";
+      let rumus = "5/9." + "(" + input + "-32)";
       let result = 5 / 9 * (input - 32);
       document.getElementById("answer").value = result;
       document.getElementById("rumus").value = rumus;
