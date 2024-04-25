fbb1107d0b853601ef094cc4d5cde8594bf8d9e3
Angelo <christianangelor94@gmail.com>
2024-04-25 13:23:37 +0700

    feat: complete calculation

--- js/script.js
+++ js/script.js
@@ -1,16 +1,57 @@
 // JS File
+const ftoc = document.getElementById("f-to-c");
+const ctof = document.getElementById("c-to-f");
+
+document.getElementById("suhu-1").addEventListener("change", function () {
+  const suhuu1 = document.getElementById("suhu-1").value;
+  if (suhuu1 === "celsius") {
+    ctof.style.display = "flex";
+    ftoc.style.display = "none";
+    document.getElementById("suhu-2").value = "fahrenheit";
+  } else if (suhuu1 === "fahrenheit") {
+    ftoc.style.display = "flex";
+    ctof.style.display = "none";
+    document.getElementById("suhu-2").value = "celsius";
+  }
+});
+
+document.getElementById("suhu-2").addEventListener("change", function () {
+  const suhuu2 = document.getElementById("suhu-2").value;
+  if (suhuu2 === "celsius") {
+    ftoc.style.display = "flex";
+    ctof.style.display = "none";
+    document.getElementById("suhu-1").value = "fahrenheit";
+  } else if (suhuu2 === "fahrenheit") {
+    ctof.style.display = "flex";
+    ftoc.style.display = "none";
+    document.getElementById("suhu-1").value = "celsius";
+  }
+});
 
 function konversi() {
-    let celsius = document.getElementById("celsius").value;
-    let rumus = "(" + celsius +".9/5)+32"
-    let result = (celsius * 9 / 5) + 32;
-    console.log(result);
-    document.getElementById("fahrenheit").value = result
-    document.getElementById("rumus").value = rumus
+  const suhu1 = document.getElementById("suhu-1").value;
+  const input = parseFloat(document.getElementById("input").value);
+  if (isNaN(input)) {
+    alert("Harap inputkan angka suhu");
+  } else {
+    if (suhu1 === "celsius") {
+      let input = document.getElementById("input").value;
+      let rumus = "(" + input + ".9/5)+32";
+      let result = (input * 9 / 5) + 32;
+      document.getElementById("answer").value = result;
+      document.getElementById("rumus").value = rumus;
+    } else if (suhu1 === "fahrenheit") {
+      let input = document.getElementById("input").value;
+      let rumus = "5 / 9." + "(" + input + " - 32)";
+      let result = 5 / 9 * (input - 32);
+      document.getElementById("answer").value = result;
+      document.getElementById("rumus").value = rumus;
+    }
+  }
 }
 
 function reset() {
-    document.getElementById("celsius").value = "";
-    document.getElementById("fahrenheit").value = "";
-    document.getElementById("rumus").value = "";
+  document.getElementById("input").value = "";
+  document.getElementById("answer").value = "";
+  document.getElementById("rumus").value = "";
 }
